dcd742c012d11f2062452274bcad6163bb8f6f37
flar2 <asegaert@gmail.com>
2016-11-01 00:31:38 -0400

    sound_control: add speaker gain
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- sound/soc/codecs/tfa98xx.c
+++ sound/soc/codecs/tfa98xx.c
@@ -2570,6 +2570,9 @@ static struct snd_soc_dai_driver tfa98xx_dai[] = {
 	},
 };
 
+#ifdef CONFIG_SOUND_CONTROL
+extern struct snd_soc_codec *tfa98xx_codec_ptr;
+#endif
 static int tfa98xx_probe(struct snd_soc_codec *codec)
 {
 	/* struct i2c_client *i2c = to_i2c_client(codec->dev); */
@@ -2594,6 +2597,9 @@ static int tfa98xx_probe(struct snd_soc_codec *codec)
 
 	tfa98xx->codec = codec;
 
+#ifdef CONFIG_SOUND_CONTROL
+	tfa98xx_codec_ptr = codec;
+#endif
 	ret = tfa98xx_load_container(tfa98xx);
 	pr_debug("Container loading requested: %d\n", ret);
 
