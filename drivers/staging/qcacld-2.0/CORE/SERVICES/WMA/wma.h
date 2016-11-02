8d3ad2504593a39b8c27f70a5c9852025672f04a
Francisco Franco <franciscofranco.1990@gmail.com>
2016-11-02 17:15:27 +0000

    qcacld-2.0: more logging bites the dust
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    (cherry picked from commit ec6d5d5951ba46e50dc6aa3c53628ec1c89cb32d)
    (cherry picked from commit 0fba7a27afeeb981bb7375ac4ca40c7f4fea3fec)
    (cherry picked from commit 456842048e040b7f8cf89cf490b8cc4c72dc9fa0)

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.h
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.h
@@ -117,24 +117,28 @@
 #define WMA_11P_CHANNEL_BEGIN           (170)
 #define WMA_11P_CHANNEL_END             (184)
 
-#define WMA_LOGD(args...) \
-	VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_DEBUG, ## args)
-#define WMA_LOGI(args...) \
-	VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_INFO, ## args)
-#define WMA_LOGW(args...) \
-	VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_WARN, ## args)
-#define WMA_LOGE(args...) \
-	VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_ERROR, ## args)
-#define WMA_LOGP(args...) \
-	VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_FATAL, ## args)
-
 #undef WMA_DEBUG_ALWAYS
 
 #ifdef WMA_DEBUG_ALWAYS
 #define WMA_LOGA(args...) \
 	VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_FATAL, ## args)
+#define WMA_LOGD(args...) \
+        VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_DEBUG, ## args)
+#define WMA_LOGI(args...) \
+        VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_INFO, ## args)
+#define WMA_LOGW(args...) \
+        VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_WARN, ## args)
+#define WMA_LOGE(args...) \
+        VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_ERROR, ## args)
+#define WMA_LOGP(args...) \
+        VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_FATAL, ## args)
 #else
 #define WMA_LOGA(args...)
+#define WMA_LOGD(args...)
+#define WMA_LOGI(args...)
+#define WMA_LOGW(args...)
+#define WMA_LOGE(args...)
+#define WMA_LOGP(args...)
 #endif
 
 #define     ALIGNED_WORD_SIZE       4
