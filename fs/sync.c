83357e02a25fadfa571a34c4a225db952ef831e0
Francisco Franco <franciscofranco.1990@gmail.com>
2016-12-14 17:30:06 +0000

    fs: sync: block fsync() operations during input boost events
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -16,13 +16,19 @@
 #include <linux/pagemap.h>
 #include <linux/quotaops.h>
 #include <linux/backing-dev.h>
+#include <linux/fsync.h>
 #include "internal.h"
 #ifdef CONFIG_ASYNC_FSYNC
 #include <linux/statfs.h>
 #endif
 
 bool fsync_enabled = true;
-module_param(fsync_enabled, bool, 0755);
+module_param(fsync_enabled, bool, 0644);
+
+void set_fsync(bool enable)
+{
+        fsync_enabled = enable;
+}
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -217,7 +223,7 @@ int vfs_fsync(struct file *file, int datasync)
 {
 	if (!fsync_enabled)
 		return 0;
-		
+
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
