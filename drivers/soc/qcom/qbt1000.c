80f07fc522561b72f3f38ce2e82005599dbd2cf7
GUYVER91 <BlackDeath1370@gmail.com>
2017-05-06 13:48:44 +0430

    qbt1000: Xiaomi changes for Mi 5s

--- drivers/soc/qcom/qbt1000.c
+++ drivers/soc/qcom/qbt1000.c
@@ -1,4 +1,5 @@
-/* Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -86,7 +87,6 @@ struct qbt1000_drvdata {
 	uint32_t	ssc_spi_port;
 	uint32_t	ssc_spi_port_slave_index;
 	struct wakeup_source w_lock;
-	struct qseecom_handle *app_handle;
 };
 #define W_LOCK_DELAY_MS (2000)
 
@@ -111,8 +111,7 @@ static int get_cmd_rsp_buffers(struct qseecom_handle *hdl,
 	*cmd_len = ALIGN(*cmd_len, 64);
 	*rsp_len = ALIGN(*rsp_len, 64);
 
-	if (((uint64_t)*rsp_len + (uint64_t)*cmd_len)
-	  > (uint64_t)g_app_buf_size)
+	if ((*rsp_len + *cmd_len) > g_app_buf_size)
 		return -ENOMEM;
 
 	*cmd = hdl->sbuf;
@@ -792,20 +791,8 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 			goto end;
 		}
 
-		if (drvdata->app_handle) {
-			dev_err(drvdata->dev, "%s: LOAD app already loaded, unloading first\n",
-				__func__);
-			rc = qseecom_shutdown_app(&drvdata->app_handle);
-			if (rc != 0) {
-				dev_err(drvdata->dev, "%s: LOAD current app failed to shutdown\n",
-					  __func__);
-				goto end;
-			}
-		}
-
 		/* start the TZ app */
-		rc = qseecom_start_app(&drvdata->app_handle,
-				app.name, app.size);
+		rc = qseecom_start_app(&app_handle, app.name, app.size);
 		if (rc == 0) {
 			g_app_buf_size = app.size;
 		} else {
@@ -814,9 +801,7 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 			goto end;
 		}
 
-		/* copy a fake app handle to user */
-		app_handle = drvdata->app_handle ?
-			(struct qseecom_handle *)123456 : 0;
+		/* copy the app handle to user */
 		rc = copy_to_user((void __user *)app.app_handle, &app_handle,
 			sizeof(*app.app_handle));
 
@@ -833,7 +818,7 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 	case QBT1000_UNLOAD_APP:
 	{
 		struct qbt1000_app app;
-		struct qseecom_handle *app_handle = 0;
+		struct qseecom_handle *app_handle;
 
 		if (copy_from_user(&app, priv_arg,
 			sizeof(app)) != 0) {
@@ -863,14 +848,14 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 		}
 
 		/* if the app hasn't been loaded already, return err */
-		if (!drvdata->app_handle) {
+		if (!app_handle) {
 			dev_err(drvdata->dev, "%s: App not loaded\n",
 				__func__);
 			rc = -EINVAL;
 			goto end;
 		}
 
-		rc = qseecom_shutdown_app(&drvdata->app_handle);
+		rc = qseecom_shutdown_app(&app_handle);
 		if (rc != 0) {
 			dev_err(drvdata->dev, "%s: App failed to shutdown\n",
 				__func__);
@@ -911,7 +896,7 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 		}
 
 		/* if the app hasn't been loaded already, return err */
-		if (!drvdata->app_handle) {
+		if (!tzcmd.app_handle) {
 			dev_err(drvdata->dev, "%s: App not loaded\n",
 				__func__);
 			rc = -EINVAL;
@@ -921,20 +906,13 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 		/* init command and response buffers and align lengths */
 		aligned_cmd_len = tzcmd.req_buf_len;
 		aligned_rsp_len = tzcmd.rsp_buf_len;
-		rc = get_cmd_rsp_buffers(drvdata->app_handle,
+		rc = get_cmd_rsp_buffers(tzcmd.app_handle,
 			(void **)&aligned_cmd,
 			&aligned_cmd_len,
 			(void **)&aligned_rsp,
 			&aligned_rsp_len);
 		if (rc != 0)
 			goto end;
-
-		if (!aligned_cmd) {
-			dev_err(drvdata->dev, "%s: Null command buffer\n",
-				__func__);
-			rc = -EINVAL;
-			goto end;
-		}
 
 		rc = copy_from_user(aligned_cmd, (void __user *)tzcmd.req_buf,
 				tzcmd.req_buf_len);
@@ -946,7 +924,7 @@ static long qbt1000_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 		}
 
 		/* send cmd to TZ */
-		rc = qseecom_send_command(drvdata->app_handle,
+		rc = qseecom_send_command(tzcmd.app_handle,
 			aligned_cmd,
 			aligned_cmd_len,
 			aligned_rsp,
@@ -1086,10 +1064,8 @@ int qbt1000_create_input_device(struct qbt1000_drvdata *drvdata)
 
 	drvdata->in_dev->evbit[0] = BIT_MASK(EV_KEY) |  BIT_MASK(EV_ABS);
 	drvdata->in_dev->keybit[BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH);
-
-	/* enable all 256 key events except to key 00 which is "KEY_RESERVED" */
-	memset(drvdata->in_dev->keybit, 0xFE,
-		   BIT_WORD(0x100)*sizeof(unsigned long));
+	drvdata->in_dev->keybit[BIT_WORD(KEY_HOMEPAGE)] =
+		BIT_MASK(KEY_HOMEPAGE);
 
 	input_set_abs_params(drvdata->in_dev, ABS_X,
 			     0,
