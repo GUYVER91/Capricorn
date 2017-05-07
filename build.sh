3815c03d83f5e20042361fd7e80ce098e2a8fb0a
GUYVER91 <BlackDeath1370@gmail.com>
2017-05-07 11:38:03 +0430

    build scriptL: switch to Linaro 6.3.1 GCC (2017.05-rc1)

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ KERNEL_DIR=$PWD
 DTBTOOL=$KERNEL_DIR/dtbTool
 cd $KERNEL_DIR
 export ARCH=arm64
-export CROSS_COMPILE="/home/guyver/Desktop/Toolchain/Linaro-v4.9.4/bin/aarch64-linux-android-"
-export LD_LIBRARY_PATH=/home/guyver/Desktop/Toolchain/Linaro-v4.9.4/lib/
-STRIP="/home/guyver/Desktop/Toolchain/Linaro-v4.9.4/bin/aarch64-linux-android-strip"
+export CROSS_COMPILE="/home/guyver/Desktop/Toolchain/Linaro-Snapshot-6.3/bin/aarch64-unknown-linux-gnueabi-"
+export LD_LIBRARY_PATH=home/guyver/Desktop/Toolchain/Linaro-Snapshot-6.3/lib/
+STRIP="/home/guyver/Desktop/Toolchain/Linaro-Snapshot-6.3/bin/aarch64-unknown-linux-gnueabi-strip"
 make clean
 make guyver_defconfig
 export KBUILD_BUILD_HOST="Ubuntu"
