d6826363edac6be82b622098b34e5e953d0df4e2
Humberto Borba <humberos@gmail.com>
2017-07-18 17:54:38 -0300

    drivers: ipa: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I0e90bd3c46552849d266668f750b5a4f33a87041

--- drivers/platform/msm/ipa/ipa_v2/ipa_uc_wdi.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa_uc_wdi.c
@@ -933,7 +933,7 @@ int ipa2_connect_wdi_pipe(struct ipa_wdi_in_params *in,
 				IPA_CPU_2_HW_CMD_WDI_TX_SET_UP :
 				IPA_CPU_2_HW_CMD_WDI_RX_SET_UP,
 				IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-				false, 10*HZ);
+				false, IPA_TIMEOUT(10));
 
 	if (result) {
 		result = -EFAULT;
@@ -1026,7 +1026,7 @@ int ipa2_disconnect_wdi_pipe(u32 clnt_hdl)
 	result = ipa_uc_send_cmd(tear.raw32b,
 				IPA_CPU_2_HW_CMD_WDI_TEAR_DOWN,
 				IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-				false, 10*HZ);
+				false, IPA_TIMEOUT(10));
 
 	if (result) {
 		result = -EFAULT;
@@ -1090,7 +1090,7 @@ int ipa2_enable_wdi_pipe(u32 clnt_hdl)
 	result = ipa_uc_send_cmd(enable.raw32b,
 		IPA_CPU_2_HW_CMD_WDI_CH_ENABLE,
 		IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-		false, 10*HZ);
+		false, IPA_TIMEOUT(10));
 
 	if (result) {
 		result = -EFAULT;
@@ -1191,7 +1191,7 @@ int ipa2_disable_wdi_pipe(u32 clnt_hdl)
 	result = ipa_uc_send_cmd(disable.raw32b,
 		IPA_CPU_2_HW_CMD_WDI_CH_DISABLE,
 		IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-		false, 10*HZ);
+		false, IPA_TIMEOUT(10));
 
 	if (result) {
 		result = -EFAULT;
@@ -1258,7 +1258,7 @@ int ipa2_resume_wdi_pipe(u32 clnt_hdl)
 	result = ipa_uc_send_cmd(resume.raw32b,
 		IPA_CPU_2_HW_CMD_WDI_CH_RESUME,
 		IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-		false, 10*HZ);
+		false, IPA_TIMEOUT(10));
 
 	if (result) {
 		result = -EFAULT;
@@ -1328,7 +1328,7 @@ int ipa2_suspend_wdi_pipe(u32 clnt_hdl)
 		result = ipa_uc_send_cmd(suspend.raw32b,
 			IPA_CPU_2_HW_CMD_WDI_CH_SUSPEND,
 			IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-			false, 10*HZ);
+			false, IPA_TIMEOUT(10));
 
 		if (result) {
 			result = -EFAULT;
@@ -1359,7 +1359,7 @@ int ipa2_suspend_wdi_pipe(u32 clnt_hdl)
 		result = ipa_uc_send_cmd(suspend.raw32b,
 			IPA_CPU_2_HW_CMD_WDI_CH_SUSPEND,
 			IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-			false, 10*HZ);
+			false, IPA_TIMEOUT(10));
 
 		if (result) {
 			result = -EFAULT;
@@ -1408,7 +1408,7 @@ int ipa_write_qmapid_wdi_pipe(u32 clnt_hdl, u8 qmap_id)
 	result = ipa_uc_send_cmd(qmap.raw32b,
 		IPA_CPU_2_HW_CMD_WDI_RX_EXT_CFG,
 		IPA_HW_2_CPU_WDI_CMD_STATUS_SUCCESS,
-		false, 10*HZ);
+		false, IPA_TIMEOUT(10));
 
 	if (result) {
 		result = -EFAULT;
