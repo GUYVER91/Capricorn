5dcfae651b41028c9faffef3e20d8cbbf365ca6b
GUYVER91 <BlackDeath1370@gmail.com>
2017-09-15 09:10:17 +0900

    hyperx: a tweaked ondemand based smart and smooth optimized Governor
    all credits to GameTheory-

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -507,6 +507,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPERX)
+extern struct cpufreq_governor cpufreq_gov_hyperx;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_hyperx)
 #endif
 
 /*********************************************************************
